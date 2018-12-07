e5efdc7ae9048d535b5946d9ec637e0d9ccfb4c5
Marc Pomar <marc@faable.com>
2018-12-07 10:40:20 +0100

    done

--- runPySpark.sh
+++ runPySpark.sh
@@ -11,17 +11,27 @@ function checkCommand {
     fi
 }
 
+
+
 # Check installed packages first
 checkCommand "git"
-checkCommand "brew"
-checkCommand "jq" "install with: brew install jq"
-checkCommand "spark-shell" "install with: brew install apache-spark"
+checkCommand "spark-shell"
 checkCommand "jupyter"
 
+# Platform dependent checks and SPARK_HOME EXPORT
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export SPARK_HOME=/home/$(whoami)/spark
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    checkCommand "brew"
+    INSTALLED_SPARK_VERSION=$(brew info --json=v1  apache-spark | jq -r ".[0].installed[0].version")
+    echo "Using installed spark: $INSTALLED_SPARK_VERSION"
+    export SPARK_HOME=/usr/local/Cellar/apache-spark/$INSTALLED_SPARK_VERSION/libexec
+else
+    echo "NOT SUPPORTED OS"
+    exit 1;
+fi
 
-INSTALLED_SPARK_VERSION=$(brew info --json=v1  apache-spark | jq -r ".[0].installed[0].version")
-echo "Using installed spark: $INSTALLED_SPARK_VERSION"
-export SPARK_HOME=/usr/local/Cellar/apache-spark/$INSTALLED_SPARK_VERSION/libexec
+# Configure pyspark
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
 export PYSPARK_PYTHON=python3
